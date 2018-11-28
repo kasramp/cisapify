a763d195518b452f829c2c9e29bcf7e5f266b434
kasramp <kasra_mp@yahoo.com>
2018-11-28 07:25:47 +0100

    #1 add missing annotation

--- src/main/java/com/madadipouya/cisapify/user/player/song/service/impl/SongServiceImpl.java
+++ src/main/java/com/madadipouya/cisapify/user/player/song/service/impl/SongServiceImpl.java
@@ -2,9 +2,11 @@ package com.madadipouya.cisapify.user.player.song.service.impl;
 
 import com.madadipouya.cisapify.user.player.song.repository.SongRepository;
 import com.madadipouya.cisapify.user.player.song.service.SongService;
+import org.springframework.stereotype.Service;
 
 import java.nio.file.Path;
 
+@Service
 public class SongServiceImpl implements SongService {
 
     private final SongRepository songRepository;

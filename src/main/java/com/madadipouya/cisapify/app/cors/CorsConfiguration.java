e89e73b05d0f6be8346c13b2310730447c7b5e33
kasramp <kasra_mp@yahoo.com>
2018-12-09 11:13:53 +0100

    Simplify Cors configuration

--- src/main/java/com/madadipouya/cisapify/app/cors/CorsConfiguration.java
+++ src/main/java/com/madadipouya/cisapify/app/cors/CorsConfiguration.java
@@ -1,20 +1,14 @@
 package com.madadipouya.cisapify.app.cors;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration {
+public class CorsConfiguration implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
     }
 }

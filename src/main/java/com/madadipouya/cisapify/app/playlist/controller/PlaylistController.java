451b9edf5f590a799e3e7cb8e8b49eb76843396e
kasramp <kasra_mp@yahoo.com>
2019-01-25 08:26:13 +0100

    #23 Add the controller skeleton for the playlist

---
+++ src/main/java/com/madadipouya/cisapify/app/playlist/controller/PlaylistController.java
@@ -0,0 +1,12 @@
+package com.madadipouya.cisapify.app.playlist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class PlaylistController {
+
+
+
+}

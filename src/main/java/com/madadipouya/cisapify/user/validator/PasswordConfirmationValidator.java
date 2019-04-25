fb2c332ef18771f28847db24834d31519cb46cc0
kasramp <kasra_mp@yahoo.com>
2019-04-25 07:43:31 +0200

    Save user profile changes to database

--- src/main/java/com/madadipouya/cisapify/user/validator/PasswordConfirmationValidator.java
+++ src/main/java/com/madadipouya/cisapify/user/validator/PasswordConfirmationValidator.java
@@ -2,6 +2,7 @@ package com.madadipouya.cisapify.user.validator;
 
 import com.madadipouya.cisapify.user.controller.ProfileController;
 import com.madadipouya.cisapify.user.metadata.ConfirmPassword;
+import com.madadipouya.cisapify.user.service.UserService;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.validation.ConstraintValidator;
@@ -9,8 +10,15 @@ import javax.validation.ConstraintValidatorContext;
 
 public class PasswordConfirmationValidator implements ConstraintValidator<ConfirmPassword, ProfileController.UserCommand> {
 
+    private final UserService userService;
+
+    PasswordConfirmationValidator(UserService userService) {
+        this.userService = userService;
+    }
+
     @Override
     public boolean isValid(ProfileController.UserCommand value, ConstraintValidatorContext context) {
-        return StringUtils.equals(value.getPassword(), value.getConfirmPassword());
+        return StringUtils.equals(value.getPassword(), value.getConfirmPassword()) ||
+                StringUtils.equals(value.getPassword(), userService.getCurrentUser().getPassword());
     }
 }

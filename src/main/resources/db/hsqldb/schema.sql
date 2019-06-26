08aee7d132a996160b9869537625a51f38420f32
kasramp <kasra_mp@yahoo.com>
2019-06-26 08:13:16 +0200

    #15 remove old scripts

--- src/main/resources/db/hsqldb/schema.sql
+++ src/main/resources/db/hsqldb/schema.sql
@@ -1,71 +1,3 @@
-DROP TABLE vet_specialties IF EXISTS;
-DROP TABLE vets IF EXISTS;
-DROP TABLE specialties IF EXISTS;
-DROP TABLE visits IF EXISTS;
-DROP TABLE pets IF EXISTS;
-DROP TABLE types IF EXISTS;
-DROP TABLE owners IF EXISTS;
-
-
-CREATE TABLE vets (
-  id         INTEGER IDENTITY PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name  VARCHAR(30)
-);
-CREATE INDEX vets_last_name ON vets (last_name);
-
-CREATE TABLE specialties (
-  id   INTEGER IDENTITY PRIMARY KEY,
-  name VARCHAR(80)
-);
-CREATE INDEX specialties_name ON specialties (name);
-
-CREATE TABLE vet_specialties (
-  vet_id       INTEGER NOT NULL,
-  specialty_id INTEGER NOT NULL
-);
-ALTER TABLE vet_specialties ADD CONSTRAINT fk_vet_specialties_vets FOREIGN KEY (vet_id) REFERENCES vets (id);
-ALTER TABLE vet_specialties ADD CONSTRAINT fk_vet_specialties_specialties FOREIGN KEY (specialty_id) REFERENCES specialties (id);
-
-CREATE TABLE types (
-  id   INTEGER IDENTITY PRIMARY KEY,
-  name VARCHAR(80)
-);
-CREATE INDEX types_name ON types (name);
-
-CREATE TABLE owners (
-  id         INTEGER IDENTITY PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name  VARCHAR_IGNORECASE(30),
-  address    VARCHAR(255),
-  city       VARCHAR(80),
-  telephone  VARCHAR(20)
-);
-CREATE INDEX owners_last_name ON owners (last_name);
-
-CREATE TABLE pets (
-  id         INTEGER IDENTITY PRIMARY KEY,
-  name       VARCHAR(30),
-  birth_date DATE,
-  type_id    INTEGER NOT NULL,
-  owner_id   INTEGER NOT NULL
-);
-ALTER TABLE pets ADD CONSTRAINT fk_pets_owners FOREIGN KEY (owner_id) REFERENCES owners (id);
-ALTER TABLE pets ADD CONSTRAINT fk_pets_types FOREIGN KEY (type_id) REFERENCES types (id);
-CREATE INDEX pets_name ON pets (name);
-
-CREATE TABLE visits (
-  id          INTEGER IDENTITY PRIMARY KEY,
-  pet_id      INTEGER NOT NULL,
-  visit_date  DATE,
-  description VARCHAR(255)
-);
-ALTER TABLE visits ADD CONSTRAINT fk_visits_pets FOREIGN KEY (pet_id) REFERENCES pets (id);
-CREATE INDEX visits_pet_id ON visits (pet_id);
-
-
--- CISAPIFY
-
 DROP TABLE users IF EXISTS;
 
 CREATE TABLE users (
@@ -99,6 +31,8 @@ CREATE TABLE songs (
 );
 CREATE INDEX idx_song_file_name ON songs (file_name);
 
+DROP TABLE roles IF EXISTS;
+
 CREATE TABLE roles (
   id INTEGER IDENTITY PRIMARY KEY,
   role VARCHAR(128) NOT NULL,
@@ -106,6 +40,8 @@ CREATE TABLE roles (
   CONSTRAINT uc_role_role_name UNIQUE (role)
 );
 CREATE INDEX idx_role_role_name on roles (role);
+
+DROP TABLE user_role IF EXISTS;
 
 CREATE TABLE user_role (
   user_id INTEGER NOT NULL,
@@ -115,6 +51,8 @@ CREATE TABLE user_role (
   CONSTRAINT fk_user_role_user_id FOREIGN KEY (user_id) REFERENCES users (id),
   CONSTRAINT fk_user_role_role_id FOREIGN KEY (role_id) REFERENCES roles (id)
 );
+
+DROP TABLE playlists IF EXISTS;
 
 CREATE TABLE playlists (
   id INTEGER IDENTITY PRIMARY KEY,
@@ -128,6 +66,8 @@ CREATE TABLE playlists (
         ON UPDATE CASCADE
 );
 
+DROP TABLE playlist_song IF EXISTS;
+
 CREATE TABLE playlist_song (
   playlist_id INTEGER NOT NULL,
   song_id INTEGER NOT NULL,
@@ -135,4 +75,3 @@ CREATE TABLE playlist_song (
   CONSTRAINT fk_playlist_song_playlist_id FOREIGN KEY (playlist_id) REFERENCES playlists (id),
   CONSTRAINT fk_playlist_song_song_id FOREIGN KEY (song_id) REFERENCES songs (id)
 );
--- END CISAPIFY
--- src/main/resources/db/mysql/schema.sql
+++ src/main/resources/db/mysql/schema.sql
@@ -6,65 +6,6 @@ ALTER DATABASE cisapify
 
 USE cisapify;
 
-CREATE TABLE IF NOT EXISTS vets (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  INDEX(last_name)
-) engine=InnoDB;
-
-CREATE TABLE IF NOT EXISTS specialties (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(80),
-  INDEX(name)
-) engine=InnoDB;
-
-CREATE TABLE IF NOT EXISTS vet_specialties (
-  vet_id INT(4) UNSIGNED NOT NULL,
-  specialty_id INT(4) UNSIGNED NOT NULL,
-  FOREIGN KEY (vet_id) REFERENCES vets(id),
-  FOREIGN KEY (specialty_id) REFERENCES specialties(id),
-  UNIQUE (vet_id,specialty_id)
-) engine=InnoDB;
-
-CREATE TABLE IF NOT EXISTS types (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(80),
-  INDEX(name)
-) engine=InnoDB;
-
-CREATE TABLE IF NOT EXISTS owners (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  address VARCHAR(255),
-  city VARCHAR(80),
-  telephone VARCHAR(20),
-  INDEX(last_name)
-) engine=InnoDB;
-
-CREATE TABLE IF NOT EXISTS pets (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30),
-  birth_date DATE,
-  type_id INT(4) UNSIGNED NOT NULL,
-  owner_id INT(4) UNSIGNED NOT NULL,
-  INDEX(name),
-  FOREIGN KEY (owner_id) REFERENCES owners(id),
-  FOREIGN KEY (type_id) REFERENCES types(id)
-) engine=InnoDB;
-
-CREATE TABLE IF NOT EXISTS visits (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  pet_id INT(4) UNSIGNED NOT NULL,
-  visit_date DATE,
-  description VARCHAR(255),
-  FOREIGN KEY (pet_id) REFERENCES pets(id)
-) engine=InnoDB;
-
-
--- CISAPIFY
-
 CREATE TABLE IF NOT EXISTS users (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   email_address VARCHAR(512) NOT NULL,
@@ -130,4 +71,3 @@ CREATE TABLE IF NOT EXISTS playlist_song (
   CONSTRAINT fk_playlist_song_playlist_id FOREIGN KEY (playlist_id) REFERENCES playlists (id),
   CONSTRAINT fk_playlist_song_song_id FOREIGN KEY (song_id) REFERENCES songs (id)
 ) engine=InnoDB;
--- END CISAPIFY

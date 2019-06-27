79267425766ba3999bad70b11dbc3bb706e91df3
kasramp <kasra_mp@yahoo.com>
2019-06-27 16:58:14 +0200

    #15 get Dropbox params as environment variable

--- util/run_container.sh
+++ util/run_container.sh
@@ -7,12 +7,14 @@ if [[ $# -eq 0 ]]
     echo -e "\t - URI of the database"
     echo -e "\t - Username of the database"
     echo -e "\t - Password of the database"
+    echo -e "\t - Dropbox App Key"
+    echo -e "\t - Dropbox App Secret"
     exit 1
 fi
 
 mkdir -p static/songs
 
-docker run --mount src=$(pwd)/static/songs,target=/user/share/cisapify/static/songs,type=bind -d -p8080:8080 -e SPRING_DATASOURCE_URL=$1 -e SPRING_DATASOURCE_USERNAME=$2 -e SPRING_DATASOURCE_PASSWORD=$3 registry.hub.docker.com/kasramp/cisapify:latest
+docker run --mount src=$(pwd)/static/songs,target=/user/share/cisapify/static/songs,type=bind -d -p8080:8080 -e SPRING_DATASOURCE_URL=$1 -e SPRING_DATASOURCE_USERNAME=$2 -e SPRING_DATASOURCE_PASSWORD=$3 -e APP_INFO_KEY=$4 -e APP_INFO_SECRET=$5 registry.hub.docker.com/kasramp/cisapify:latest
 
 
 echo -e "Successfully ran the container"

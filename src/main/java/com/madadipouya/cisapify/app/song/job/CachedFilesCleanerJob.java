edca27f6269e372d50f7f177b06097a78207bb69
kasramp <kasra_mp@yahoo.com>
2019-07-11 17:47:52 +0200

    #40 remove cached files after 2 hours

---
+++ src/main/java/com/madadipouya/cisapify/app/song/job/CachedFilesCleanerJob.java
@@ -0,0 +1,48 @@
+package com.madadipouya.cisapify.app.song.job;
+
+import com.madadipouya.cisapify.i18n.service.I18nService;
+import com.madadipouya.cisapify.util.SongUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+
+@Component
+public class CachedFilesCleanerJob {
+
+    private static final Logger logger = LoggerFactory.getLogger(CachedFilesCleanerJob.class);
+
+    private final I18nService i18nService;
+
+    public CachedFilesCleanerJob(I18nService i18nService) {
+        this.i18nService = i18nService;
+    }
+
+    // Run every 2 hours
+    @Scheduled(cron = "0 */2 * * * ?")
+    protected void deleteSongsInTempDirectoryOlderThanTwoHours() {
+        logger.info(i18nService.getMessage("song.job.cachedFilesCleaner.start"));
+        List<Path> songsToDelete = SongUtil.getSongsInTempDirectoryOlderThan(getTwoHoursAgoTime());
+        songsToDelete.forEach(this::deleteSong);
+    }
+
+    private Instant getTwoHoursAgoTime() {
+        return Instant.now().minus(Duration.ofHours(2));
+    }
+
+    private void deleteSong(Path song) {
+        try {
+            Files.deleteIfExists(song);
+        } catch (IOException fileDeletionException) {
+            logger.warn(i18nService.getMessage("song.job.cachedFilesCleaner.fileDeletionFailure",
+                    song.getFileName()), fileDeletionException);
+        }
+    }
+}
--- src/main/java/com/madadipouya/cisapify/util/SongUtil.java
+++ src/main/java/com/madadipouya/cisapify/util/SongUtil.java
@@ -2,12 +2,26 @@ package com.madadipouya.cisapify.util;
 
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.RegExUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.time.Instant;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class SongUtil {
+
+    private static final Logger logger = LoggerFactory.getLogger(SongUtil.class);
+
+    private static final String TMP_DIRECTORY = "/tmp";
 
     private static final Set<String> AUDIO_FORMATS = Set.of("aac", "flac", "m4a", "m4b", "m4p", "mp3",
             "mpc", "ogg", "oga", "wav", "wma", "webm");
@@ -26,5 +40,21 @@ public class SongUtil {
 
     public static String generateRandomSongFilename(String songFileName) {
         return String.format("%s.%s", UUID.randomUUID().toString(), FilenameUtils.getExtension(songFileName));
+    }
+
+    public static List<Path> getSongsInTempDirectoryOlderThan(Instant time) {
+        return getAllSongsInTempDirectory().stream().filter(song -> {
+            try {
+                return Files.readAttributes(song, BasicFileAttributes.class).lastAccessTime().toInstant().isBefore(time);
+            } catch (IOException ioException) {
+                logger.warn("Failed to get last modified time of file", ioException);
+                return false;
+            }
+        }).collect(Collectors.toList());
+    }
+
+    private static List<Path> getAllSongsInTempDirectory() {
+        File[] songs = new File(TMP_DIRECTORY).listFiles((dir, file) -> isAudioFile(file));
+        return songs != null ? List.of(songs).stream().map(File::toPath).collect(Collectors.toList()) : List.of();
     }
 }
